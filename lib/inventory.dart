0f6065251e498db8a9ec3e02a48f7a8657379e04
Ben Chuang <bschuang@users.noreply.github.com>
2019-03-08 01:22:16 -0500

    removed cloud functions dependency

--- lib/inventory.dart
+++ lib/inventory.dart
@@ -3,7 +3,6 @@ import 'package:rpg/skills.dart';
 import 'package:rpg/character.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rpg/battle.dart';
--- lib/newChar.dart
+++ lib/newChar.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rpg/skills.dart';
-import 'package:rpg/character.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rpg/battle.dart';
 import 'package:rpg/main.dart';
 
 class NewChar extends StatefulWidget {
